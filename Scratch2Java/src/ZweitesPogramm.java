23ba89ffb55c51d5c8455982d0ccfa7fb2c8a0ae
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-22 09:12:22 +0200

    ZweitesProgramm

---
+++ Scratch2Java/src/ZweitesPogramm.java
@@ -0,0 +1,17 @@
+public class ZweitesPogramm {
+    public static void  main(String[] args) {
+        int zahl1;
+        int zahl2;
+        zahl1 = 5;
+        zahl2 = 4;
+        int ergebniss;
+        ergebniss = zahl1+zahl2;
+
+        System. out .println  (ergebniss);
+
+
+
+
+    }
+
+}
