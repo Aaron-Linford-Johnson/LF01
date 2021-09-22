485a2305a99850b7a047d881c7d536cdb4596ac4
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-22 10:06:59 +0200

    ZweitesProgramm

--- Scratch2Java/src/ZweitesPogramm.java
+++ Scratch2Java/src/ZweitesPogramm.java
@@ -1,13 +1,16 @@
 public class ZweitesPogramm {
     public static void  main(String[] args) {
+        // Dekleration
         int zahl1;
         int zahl2;
+        // Initialisierung (erstmalige Wertzuweisung) bzw. Wertzuweisung
         zahl1 = 5;
         zahl2 = 4;
-        int ergebniss;
-        ergebniss = zahl1+zahl2;
+        // Rechenen
+        int ergebnis;
+        ergebnis = zahl1+zahl2;
 
-        System. out .println  (ergebniss);
+        System. out .println  ("Das Ergebnis lautet: " + ergebnis);
 
 
 
