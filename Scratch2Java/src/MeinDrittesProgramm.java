ed13612029e512cbeec83ccd90763f933a13c9b8
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-23 15:28:30 +0200

    MeinDrittesProgramm

---
+++ Scratch2Java/src/MeinDrittesProgramm.java
@@ -0,0 +1,16 @@
+public class MeinDrittesProgramm {
+    public static void main(String[] args) {
+        // Dekleration
+        int grundseite;
+        int hoehe;
+        // Initialisierung (erstmalige Wertzuweisung) bzw. Wertzuweisung
+        grundseite = 6;
+        hoehe = 4;
+        // Rechenen
+        int ergebnis;
+        ergebnis = grundseite*hoehe/2;
+        // Ausgabe des Ergebnisses
+        System.out.println("Das Ergebnis ist g*h/2");
+        System.out.println(grundseite +" * " +hoehe +" / 2 = "+ ergebnis);
+    }
+}
