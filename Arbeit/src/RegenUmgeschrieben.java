a9cf0b40a5b50b079ba61645aabb3130157a2e1f
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-27 09:27:17 +0200

    RegenUmgeschrieben

--- Arbeit/src/RegenUmgeschrieben.java
+++ Arbeit/src/RegenUmgeschrieben.java
@@ -4,21 +4,20 @@ import java.io.InputStreamReader;
 
 public class RegenUmgeschrieben {
     public static void main(String[] args) throws IOException {
-        System.out.println(" Wie ist die Regenmenge im April?");
-        double regenImApril = 13.4;
-        double regenImMai = 14.6;
-        double regenImJuni = 8.3;
-
-        double mittelwert = (regenImApril + regenImMai + regenImJuni) /3;
-
-        System.out.println("Regen im April\t: \t"+regenImApril);
-        System.out.println("Regen im Mai  \t: \t"+regenImMai);
-        System.out.println("Regen im Juni \t: \t"+regenImJuni);
-        System.out.println("Mittelwert    \t: \t"+mittelwert);
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String eingabe = br.readLine();
-        double april = Double.parseDouble(april.trim())
+        System.out.println("Niederschlagsmenge im Monat April?");
+        double april = Double.parseDouble(eingabe.trim().replace(',','.'));
+        System.out.println("Niederschlagsmenge im Monat Mai?");
+        double mai = Double.parseDouble(eingabe.trim().replace(',','.'));
+        System.out.println("Niederschlagsmenge im Monat Juni?");
+        double juni = Double.parseDouble(eingabe.trim().replace(',','.'));
+
+        double durchschnitt = april + mai + juni/3;
+
+        System.out.println("Die durchschnittsmenge Niederschlagsmenge für die drei Monate beträgt: "+durchschnitt);
+
 
     }
 
