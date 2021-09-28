d0c639586e29a2536c313ba4834579596fedab46
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-28 13:01:40 +0200

    RegenUmgeschrieben

--- Scratch2Java/src/RegenUmgeschrieben.java
+++ Scratch2Java/src/RegenUmgeschrieben.java
@@ -6,17 +6,26 @@ public class RegenUmgeschrieben {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String eingabe = br.readLine();
+
         System.out.println("Niederschlagsmenge im Monat April?");
+        String eingabe = br.readLine();
         double april = Double.parseDouble(eingabe.trim().replace(',','.'));
+
         System.out.println("Niederschlagsmenge im Monat Mai?");
+        eingabe = br.readLine();
         double mai = Double.parseDouble(eingabe.trim().replace(',','.'));
+
         System.out.println("Niederschlagsmenge im Monat Juni?");
+        eingabe = br.readLine();
         double juni = Double.parseDouble(eingabe.trim().replace(',','.'));
 
-        double durchschnitt = april + mai + juni/3;
+        double durchschnitt = (april + mai + juni)/3;
 
         System.out.println("Die durchschnittsmenge Niederschlagsmenge für die drei Monate beträgt: "+durchschnitt);
+        //April 13.4
+        //Mai 14.6
+        //Juni 8.3
+        //Durchschnitt 12.1
 
 
     }
