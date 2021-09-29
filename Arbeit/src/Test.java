46b4420c0503b407f800ff68513ffdded2fc3447
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-09-29 11:12:58 +0200

    Test

---
+++ Arbeit/src/Test.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Bist du bereit für den Test?");
+        String eingabe = br.readLine();
+        String antworten = br.readLine();
+
+        int Punktzahl;
+
+        double A1 = Double.parseDouble(antworten.trim().replace(',','.'));
+
+        System.out.println("1 + 1 =");
+        System.out.println( + "= 1");
+        System.out.println(b+ "= 9999999");
+
+        if (a == 1) {
+            Punktzahl = +1;
+        } else
+            if ()
+
+
+
+
+    }
+
+}
+
