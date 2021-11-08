5a13ff8f3eb5075294944006565358108d49ddc1
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-11-08 08:53:53 +0100

    GeometrischeReihe

---
+++ Schleifen/src/GeometrischeReihe.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class GeometrischeReihe {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Geben sie die Endzahl ein:");
+        int ende = IO.readInt();
+
+        double gReihe;
+
+        for (int i = 1; i <= ende; i++) {
+            gReihe = (1/Math.pow(2,i));
+            System.out.println(gReihe);
+
+        }
+
+    }
+}
