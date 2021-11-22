00d6e8da57f32ead63884b4dd5b3febff0face4c
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-11-22 10:16:10 +0100

    Temperatur

--- Arbeit/src/Temperatur.java
+++ Arbeit/src/Temperatur.java
@@ -6,35 +6,62 @@ public class Temperatur {
     public static void main(String[] args) throws IOException {
 
         System.out.print("Bitte geben sie die Temperatur an Tag 1 ein: ");
-        double tagEins = IO.readDouble();
+        double tag1 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 2 ein: ");
-        double tagZwei = IO.readDouble();
+        double tag2 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 3 ein: ");
-        double tagDrei = IO.readDouble();
+        double tag3 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 4 ein: ");
-        double tagVier = IO.readDouble();
+        double tag4 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 5 ein: ");
-        double tagFuenf = IO.readDouble();
+        double tag5 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 6 ein: ");
-        double tagSechs = IO.readDouble();
+        double tag6 = IO.readDouble();
 
         System.out.print("Bitte geben sie die Temperatur an Tag 7 ein: ");
-        double tagSieben = IO.readDouble();
+        double tag7 = IO.readDouble();
 
-        double mittelwert = (tagEins + tagZwei + tagDrei +tagVier +tagFuenf + tagSechs + tagSieben) /7;
+        double mittelwert = (tag1 + tag2 + tag3 +tag4 +tag5 + tag6 + tag7) /7;
 
-        double [] zahlenArray = {tagEins,tagZwei,tagDrei,tagVier,tagFuenf,tagSechs,tagSieben};
-        Arrays.sort(zahlenArray);
+        double x;
 
-        double max = zahlenArray[zahlenArray.length-1];
+        if (tag1 > tag2){
+
+            x = tag1;
+        }
+        else {
+
+            x = tag2;
+        }
+        if (tag3 > x) {
+
+            x = tag3;
+        }
+        if (tag4 > x){
+
+            x = tag4;
+        }
+        if (tag5 > x){
+
+            x = tag5;
+        }
+        if (tag6 > x){
+
+            x = tag6;
+        }
+        if (tag7 > x){
+
+            x = tag7;
+        }
+
 
         System.out.println("Der Mittelwert beträgt: " +mittelwert);
-        System.out.println("Die maximale Temperatur beträgt: " + max);
+        System.out.println("Die maximale Temperatur beträgt: " + x);
 
 
     }
