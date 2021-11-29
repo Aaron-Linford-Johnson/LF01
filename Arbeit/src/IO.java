7c995e92a891bca1c8c1755193817d8f3001ac16
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-11-29 09:23:24 +0100

    IO

--- Arbeit/src/IO.java
+++ Arbeit/src/IO.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class IO {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    //String
     /**
      * Liest einen Text von der Konsole ein.
      * @return Text
@@ -18,6 +19,34 @@ public class IO {
         //return eingabe;
     }
 
+    /**
+     * Liest ein eindimensionales Array mit Text von der Konsole ein
+     * @return eindimensionales Array mit String
+     * @throws IOException
+     */
+    public static String[] readArrayString() throws IOException {
+        System.out.print("Wie groß soll das Array sein: ");
+        int n = IO.readInt();
+
+        return readStringArray(n);
+    }
+
+    /**
+     * Liest ein eindimensionales Array mit Text von der Konsole ein
+     * @param n
+     * @return eindimensionales Array mit String
+     * @throws IOException
+     */
+    public static String[] readStringArray(int n) throws IOException {
+        String[] array = new String[n];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Bitte den " + (i + 1) + " Werte eingeben: ");
+            array[i] = IO.readString();
+        }
+        return array;
+    }
+
+    //Integer
     /**
      * Liest eine Ganzzahl in der Konsole ein
      * @return Ganzzahl
@@ -36,6 +65,35 @@ public class IO {
     }
 
     /**
+     * Liest ein eindimensionales Array mit Ganzzahl von der Konsole ein
+     * @return eindimensionales Array mit Ganzzahl
+     * @throws IOException
+     */
+    public static int[] readIntArray() throws IOException {
+
+        System.out.println("Wie viele Werte sollen erfasst werden: ");
+        int n = IO.readInt();
+
+        return readArrayInt(n);
+    }
+
+    /**
+     * Liest ein eindimensionales Array mit Ganzzahl von der Konsole ein (erwartet int)
+     * @param n
+     * @return eindimensionales Array mit Ganzzahl
+     * @throws IOException
+     */
+    public static int[] readArrayInt(int n) throws IOException {
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Bitte den " + (i + 1) + " Wert eingeben: ");
+            array[i] = IO.readInt();
+        }
+        return array;
+    }
+
+    //Double
+    /**
      * Liest eine Nachkommazahl in der Konsole ein
      * @return Nachkommazahl
      * @throws IOException
@@ -52,6 +110,35 @@ public class IO {
     }
 
     /**
+     * Liest ein eindimensionales Array mit Nachkommazahl von der Konsole ein
+     * @return eindimensionales Array mit Nachkommastelle
+     * @throws IOException
+     */
+    public static double[] readDoubleArray() throws IOException {
+        System.out.println("Wie viele Werte sollen erfasst werden: ");
+        int n = IO.readInt();
+
+       return readDoubleArray(n);
+    }
+
+    /**
+     * Liest ein eindimensionales Array mit Nachkommazahl von der Konsole ein (erwartet int)
+     * @param n
+     * @return eindimensionales Array mit Nachkommastelle
+     * @throws IOException
+     */
+    public static double[] readDoubleArray(int n) throws IOException {
+        double[] array = new double[n];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Bitte den " + (i + 1) + " Wert eingeben: ");
+            array[i] = IO.readDouble();
+        }
+        return array;
+    }
+
+    //Boolean
+    /**
      * Gibt zurück ob eingabe True oder False ist
      * @return True oder False
      * @throws IOException
@@ -61,15 +148,15 @@ public class IO {
             String eingabe = IO.readString();
             if (eingabe.equalsIgnoreCase("Ja")
                     || (eingabe.equalsIgnoreCase("j"))
-                    || (eingabe.equalsIgnoreCase("Yes"))){
+                    || (eingabe.equalsIgnoreCase("Yes"))) {
                 return true;
             }
-        if (eingabe.equalsIgnoreCase("Nein")
-                || (eingabe.equalsIgnoreCase("n"))
-                || (eingabe.equalsIgnoreCase("No"))){
-            return false;
-        }
-        System.out.print("Ungültige Eingabe! Bitte erneut versuchen: ");
+            if (eingabe.equalsIgnoreCase("Nein")
+                    || (eingabe.equalsIgnoreCase("n"))
+                    || (eingabe.equalsIgnoreCase("No"))) {
+                return false;
+            }
+            System.out.print("Ungültige Eingabe! Bitte erneut versuchen: ");
         }
     }
 }
