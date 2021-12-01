976dfcdddf8f29fd05fb3c08e16ca5ae38fad0cf
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-12-01 08:08:13 +0100

    Maxima

--- Aufgabensammlung/src/Maxima.java
+++ Aufgabensammlung/src/Maxima.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class MaximaTest {
+public class Maxima {
     public static void main(String[] args) throws IOException {
 
             double[] a = IO.readDoubleArray();
