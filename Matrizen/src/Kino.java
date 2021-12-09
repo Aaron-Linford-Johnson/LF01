5a2929af0599042561677a05567b258f5479aa4a
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2021-12-09 15:30:33 +0100

    Kino

--- Matrizen/src/Kino.java
+++ Matrizen/src/Kino.java
@@ -2,11 +2,24 @@ import java.io.IOException;
 
 public class Kino {
     public static void main(String[] args) throws IOException {
-        String[][] saal = erzeugenSaal();
-        anzeigenSaal(saal);
-        reservierenPlatz(saal);
-        stornierenPlatz(saal);
+        boolean wiederholen;
+       do {
 
+           String[][] saal = erzeugenSaal();
+           anzeigenSaal(saal);
+           zeigeMenue();
+           String wahl = IO.readString();
+
+           if (wahl.equalsIgnoreCase("r")) {
+               reservierenPlatz(saal);
+               anzeigenSaal(saal);
+           } else if (wahl.equalsIgnoreCase("s")) {
+               stornierenPlatz(saal);
+               anzeigenSaal(saal);
+           }
+           System.out.println("Wollen sie einen weiteren Platz Reservieren/Stornieren?");
+           wiederholen = IO.readBoolean();
+       } while (wiederholen);
     }
     private static String[][] erzeugenSaal() throws IOException {
         System.out.print("Wie viele Reihen: ");
@@ -30,11 +43,10 @@ public class Kino {
                 System.out.print(saal[i][j] + "\t");
             }
         }
+        System.out.println();
     }
     private static void reservierenPlatz(String[][] saal) throws IOException {
-        boolean wiederholen;
 
-        do {
             System.out.println();
             System.out.println("Welcher Platz soll reserviert werden: ");
             System.out.print("Geben sie die Reihe an: ");
@@ -56,57 +68,55 @@ public class Kino {
                     System.out.print(saal[i][j] + "\t");
                 }
             }
-            System.out.println("Wollen sie einen weiteren Platz reservieren?");
-            wiederholen = IO.readBoolean();
-        } while (wiederholen);
     }
     private static void stornierenPlatz(String[][] saal) throws IOException {
-        boolean wiederholen;
+        System.out.println("Wollen sie eine Reservierung stornieren");
 
-       do {
-           System.out.println("Wollen sie eine Reservierung stornieren");
+        boolean stornieren = IO.readBoolean();
 
-           boolean stornieren = IO.readBoolean();
+        if (stornieren) {
+            System.out.println("Aktueller Belegungsplan:");
 
-           if (stornieren) {
-               System.out.println("Aktueller Belegungsplan:");
+            for (int i = 0; i < saal.length; i++) {
+                System.out.println();
+                for (int j = 0; j < saal[i].length; j++) {
 
-               for (int i = 0; i < saal.length; i++) {
-                   System.out.println();
-                   for (int j = 0; j < saal[i].length; j++) {
+                }
+            }
+            for (int i = 0; i < saal.length; i++) {
+                System.out.println();
+                for (int j = 0; j < saal[i].length; j++) {
+                    System.out.print(saal[i][j] + "\t");
+                }
+            }
+        }
+        System.out.println("Welcher Platz soll stoniert werden?");
 
-                   }
-               }
-               for (int i = 0; i < saal.length; i++) {
-                   System.out.println();
-                   for (int j = 0; j < saal[i].length; j++) {
-                       System.out.print(saal[i][j] + "\t");
-                   }
-               }
-           }
-           System.out.println("Welcher Platz soll stoniert werden?");
+        System.out.print("Geben sie die Reihe an: ");
+        int rReihe = IO.readInt();
 
-           System.out.print("Geben sie die Reihe an: ");
-           int rReihe = IO.readInt();
+        System.out.print("Geben sie den Platz in der Reihe ein: ");
+        int rPlatz = IO.readInt();
 
-           System.out.print("Geben sie den Platz in der Reihe ein: ");
-           int rPlatz = IO.readInt();
+        for (int i = 0; i < saal.length; i++) {
+            System.out.println();
+            for (int j = 0; j < saal[i].length; j++) {
+                saal[rReihe][rPlatz] = "0";
 
-           for (int i = 0; i < saal.length; i++) {
-               System.out.println();
-               for (int j = 0; j < saal[i].length; j++) {
-                   saal[rReihe][rPlatz] = "0";
+            }
+        }
+        for (int i = 0; i < saal.length; i++) {
+            System.out.println();
+            for (int j = 0; j < saal[i].length; j++) {
+                System.out.print(saal[i][j] + "\t");
+            }
+        }
+    }
+    private static void zeigeMenue() throws IOException {
+        System.out.println("Was wollen sie tun?");
+        System.out.println("[R] für Reservieren");
+        System.out.println("[S] für Stornieren");
+        System.out.println("[B] für Beenden");
 
-               }
-           }
-           for (int i = 0; i < saal.length; i++) {
-               System.out.println();
-               for (int j = 0; j < saal[i].length; j++) {
-                   System.out.print(saal[i][j] + "\t");
-               }
-           }
-           System.out.println("Wollen sie einen weiteren Platz Stornieren?");
-           wiederholen = IO.readBoolean();
-       } while (wiederholen);
     }
 }
